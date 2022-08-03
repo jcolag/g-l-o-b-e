5aa250b2c9096d4921c8e6b3e7eace67bcba62b5
John Colagioia <john@colagioia.net>
2022-08-03 06:40:16 -0400

    Add a pseudo-random number generator

--- run.js
+++ run.js
@@ -213,3 +213,20 @@ function cyrb128(str) {
 
   return [(h1^h2^h3^h4)>>>0, (h2^h1)>>>0, (h3^h1)>>>0, (h4^h1)>>>0];
 }
+
+function sfc32(a, b, c, d) {
+  return function() {
+    a >>>= 0;
+    b >>>= 0;
+    c >>>= 0;
+    d >>>= 0;
+    var t = (a + b) | 0;
+    a = b ^ b >>> 9;
+    b = c + (c << 3) | 0;
+    c = (c << 21 | c >>> 11);
+    d = d + 1 | 0;
+    t = t + d | 0;
+    c = c + t | 0;
+    return (t >>> 0) / 4294967296;
+  }
+}
